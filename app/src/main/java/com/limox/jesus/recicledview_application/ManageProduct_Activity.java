3c9006b713402d27618f3fcbbf11699085f81646
JesusLX <jpmx96@gmail.com>
2016-10-27 11:53:43 +0200

    Implementado menu, y boton para ordenar los items de la lista

---
+++ app/src/main/java/com/limox/jesus/recicledview_application/ManageProduct_Activity.java
@@ -0,0 +1,13 @@
+package com.limox.jesus.recicledview_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManageProduct_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_product);
+    }
+}
